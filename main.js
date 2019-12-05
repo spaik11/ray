42286b6b9065525d366532b3fdd261b57e1a48be
Sung Paik <sung.paik@codeimmersives.com>
2019-12-05 16:01:52 -0500

    almost done with exercise

--- main.js
+++ main.js
@@ -1,5 +1,41 @@
 const Ray = () => {
-  
+  return {
+    length: 0,
+
+    push: function(str) {
+      this[this.length] = str;
+
+      this.length++;
+    },
+
+    pop: function() {
+      const lastItem = this[this.length - 1];
+      delete this[this.length - 1];
+      this.length--;
+      
+      return lastItem;
+    },
+
+    unshift: function(str) {
+      for (let i = this.length; i > 0; i--) {
+        this[i] = this[i - 1]
+      }
+      this[0] = str;
+      this.length++;
+    },
+
+    shift: function() {
+      const firstItem = this[0];
+      delete this[0];
+      for (let i = 0; i < this.length; i++) {
+        this[i] = this[i - 1]
+      }
+
+      delete this[this.length - 1];
+      this.length--;
+      return firstItem;
+    }
+  };
 }
 
 module.exports = {
